a2aaae686cb2489097ddd393fa1a40ea2c81fa65
Gabriek0 <gabriel.sanches170@gmail.com>
2022-09-03 22:32:29 -0300

    feat: request to api/images and add returns to isLoading and isError state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,19 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+export interface Image {
+  id: string;
+  ts: number;
+  url: string;
+  title: string;
+  description: string;
+}
+
+export interface GetImageResponse {
+  after: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
   const {
     data,
@@ -17,26 +30,41 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery(
-    'images',
+    ['images'],
     // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }): Promise<GetImageResponse> => {
+      const { data } = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+      return data;
+    },
+    {
+      // TODO GET AND RETURN NEXT PAGE PARAM
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    const dataFormatted = data?.pages.map(page => page.data).flat();
+
+    return dataFormatted;
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
   // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        <CardList />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
